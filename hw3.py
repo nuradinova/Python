61311b403603d5e87f91a1c99b7b1209a391c64a
nuradinova <118282789+nuradinova@users.noreply.github.com>
2022-12-05 16:45:55 +0600

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,29 @@
+import requests
+
+
+#FIRST TASK----_--------------------------------------
+
+# url = input("Введи ссылку: ")
+# name = input("Введи начало имени картинки:")
+# amount = int(input("Введи количество загружаемых картинок:"))
+
+# for i in range(1, amount):
+#     headers = {
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+#                       'Chrome/102.0.0.0 Safari/537.36'
+#     }
+#     response = requests.get(url=url, headers=headers)
+#     image_name = f"temp/{name}{i}.jpg"
+#     with open(image_name, "wb") as opened_file:
+#         opened_file.write(response.content)
+
+
+
+
+#SECOND TASK iS HEEEEEEERE-------------------------------------------------------
+#url = requests.get(url="https://jsonplaceholder.typicode.com/posts/1")
+
+#with open("1.json", "w", encoding="UTF-8") as opened_file:
+    #opened_file.write(url.text)
+
+
